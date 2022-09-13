2e1608c7ef95b38d38d6d23f22008e69200c72f5
aliciaketzueki <aliciaketzueki@gmail.com>
2022-09-13 15:31:57 +0300

    updates

--- test-webpush/firebase_subscribe.js
+++ test-webpush/firebase_subscribe.js
@@ -9,6 +9,26 @@ firebase.initializeApp({
 if ('Notification' in window) {
     var messaging = firebase.messaging();
 
+    messaging.onMessage(function(payload) {
+        console.log('Message received. ', payload);
+
+        // регистрируем пустой ServiceWorker каждый раз
+        // navigator.serviceWorker.register('firebase-messaging-sw.js');
+        navigator.serviceWorker.register('messaging-sw.js');
+
+        // запрашиваем права на показ уведомлений если еще не получили их
+        Notification.requestPermission(function(result) {
+            if (result === 'granted') {
+                navigator.serviceWorker.ready.then(function(registration) {
+                    // теперь мы можем показать уведомление
+                    return registration.showNotification(payload.notification.title, payload.notification);
+                }).catch(function(error) {
+                    console.log('ServiceWorker registration failed', error);
+                });
+            }
+        });
+    });
+
     // пользователь уже разрешил получение уведомлений
     // подписываем на уведомления если ещё не подписали
     if (Notification.permission === 'granted') {
